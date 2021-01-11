0afac2788dadfd163651fedc22a44aa01e5ae26c
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-01-11 21:50:47 +0300

    travis

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,27 +82,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'TRAVIS' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE':   'django.db.backends.postgresql_psycopg2',
-            'NAME':     'test_db',
-            'USER':     'postgres',
-            'PASSWORD': '',
-            'HOST':     'localhost',
-            'PORT':     '5432',
-        }
-    }
-
-DATABASES = {
+'''DATABASES = {
  'default': {
  'ENGINE': 'django.db.backends.postgresql_psycopg2',
  'NAME': 'vacancy',
  'USER': 'postgres',
  'PASSWORD': '12',
  'HOST': 'localhost',
- 'PORT': '5432',
+ 'PORT': '',
  }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
