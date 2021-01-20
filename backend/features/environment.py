edf1c0755cf53298cae302104c7283b9d78da31d
oljakon <41132114+oljakon@users.noreply.github.com>
2021-01-20 23:03:04 +0300

    update environment

--- backend/features/environment.py
+++ backend/features/environment.py
@@ -1,38 +1,3 @@
-'''import os
-import django
-from django.test.runner import DiscoverRunner
-from django.test.testcases import TestCase
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
-
-
-def before_all(context):
-    import django
-    django.setup()
-    from django.contrib.auth.models import User
-    context.test_runner = DiscoverRunner()
-    context.test_runner.setup_test_environment()
-    context.old_db_config = context.test_runner.setup_databases()
-
-
-def after_all(context):
-    context.test_runner.teardown_databases(context.old_db_config)
-    context.test_runner.teardown_test_environment()
-
-
-def before_scenario(context, scenario):
-    context.test = TestCase()
-    context.test.setUpClass()
-
-
-def after_scenario(context, scenario):
-    context.test.tearDownClass()
-    del context.test
-
-
-def django_ready(context):
-    context.django = True'''
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
